b7bf7395ea4da1f152929a7a3202f70e3f53e981
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-04-20 14:16:00 +0100

    commit

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ newRecord(Data);
 
 function readData(){
   var Data ={};
-  Data["serial-no"]=document.getElementById("serial-no").value;
-  Data["product-id"]=document.getElementById("product-id").value;
-  Data["product"]= document.getElementById("product").value
-  Data["payment-type"]= document.getElementById("payment-type").value
-  Data["amount"]=document.getElementById("amount").value
+  Data["serial-no"] = document.getElementById("serial-no").value;
+  Data["product-id"] = document.getElementById("product-id").value;
+  Data["product"] = document.getElementById("product").value
+  Data["payment-type"] = document.getElementById("payment-type").value
+  Data["amount"] = document.getElementById("amount").value
   return Data;
 }
+
 function newRecord(data){
   var table = document.getElementById("product-head").getElementsByTagName('tbody')[0];
   var row = table.insertRow(table.length);
