3435a87d92962983fa3cb984f9487e97aef3717a
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-04-20 16:50:33 +0100

    no edit yet

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-function onFormSubmit(){
+function onFormSubmit(event){
+event.preventDefault()
+
 var Data= readData();
 newRecord(Data);
+// console.log(Data,);
+
 }
 
 function readData(){
   var Data ={};
-  Data["serial-no"] = document.getElementById("serial-no").value;
-  Data["product-id"] = document.getElementById("product-id").value;
-  Data["product"] = document.getElementById("product").value
-  Data["payment-type"] = document.getElementById("payment-type").value
-  Data["amount"] = document.getElementById("amount").value
+  Data["serialNo"] = document.getElementById("serialNo").value;
+  Data["productId"] = document.getElementById("productId").value;
+  Data["product"] = document.getElementById("product").value;
+  Data["paymentType"] = document.getElementById("paymentType").value;
+  Data["amount"] = document.getElementById("amount").value;
+  // console.log(Data);
   return Data;
 }
 
 function newRecord(data){
-  var table = document.getElementById("product-head").getElementsByTagName('tbody')[0];
+  var table = document.getElementById("productHead").getElementsByTagName('tbody')[0];
   var row = table.insertRow(table.length);
-  firstCell = row.insertCell(0);
-  firstCell.innerHTML = data.product-id;
-  secondCell = row.insertCell(1);
-  secondCell.innerHTML = data.product;
-  thirdCell = row.insertCell(2);
-  thirdCell.innerHTML = data.payment-type;
-  fourthCell = row.insertCell(3);
-  fourthCell.innerHTML = data.amount;
-  fifthCell = row.insertCell(4);
-  fifthCell.innerHTML = `<a>Edit</a>
-                        <a>Delete</a>`;
+  Cell1 = row.insertCell(0);
+  Cell1.innerHTML = data.serialNo;
+
+  Cell2 = row.insertCell(1);
+  Cell2.innerHTML = data.productId;
+
+  Cell3 = row.insertCell(2);
+  Cell3.innerHTML = data.product;
+
+ Cell4 = row.insertCell(3);
+  Cell4.innerHTML = data.paymentType;
+
+  Cell5 = row.insertCell(4);
+  Cell5.innerHTML = data.amount;
+
+  Cell6 = row.insertCell(5);
+ Cell6.innerHTML = `<a href="#" onclick= "onEdit(this)">Edit</a>
+                    <a  href="#">Delete</a>`;
+}
+function reset(){
+  document.getElementById("serialNo").value ="";
+  document.getElementById("productId").value ="";
+  document.getElementById("product").value ="";
+  document.getElementById("paymentType").value ="";
+  document.getElementById("amount").value ="";
+}
+
+function onEdit(td){
+  selectedRow= td.parentElement.parentElement;
+  document.getElementById("serialNo").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("productId").value =selectedRow.cells[1].innerHTML;
+  document.getElementById("product").value =selectedRow.cells[2].innerHTML;
+  document.getElementById("paymentType").value =selectedRow.cells[3].innerHTML;
+  document.getElementById("amount").value =selectedRow.cells[4].innerHTML;
 }
